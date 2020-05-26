36c2db7ec239222fa8fb27f12bb9d99b732cab02
Saravanan J <60504057+saravanannnnn@users.noreply.github.com>
2020-05-26 22:11:45 +0530

    Add files via upload

---
+++ Logic-Building/Day1/PosOrNeg.java
@@ -0,0 +1,23 @@
+package Day1;
+
+import java.util.Scanner;
+
+public class PosOrNeg {
+
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		int N;
+		N=s.nextInt();
+		if(N>0) {
+			System.out.println("Positive");
+		}
+		else if(N<0) {
+			System.out.println("Negative");
+		}
+		else {
+			System.out.println("Zero");
+		}
+		s.close();
+	}
+
+}
---
+++ Logic-Building/Day1/PrintBackwards.java
@@ -0,0 +1,8 @@
+package Day1;
+public class PrintBackwards {
+	public static void main(String[] args) {
+		for(int i=100;i>0;i=i-3) {
+			System.out.println(i);
+		}
+	}
+}
