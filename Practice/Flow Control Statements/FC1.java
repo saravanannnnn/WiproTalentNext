dea38959f4c6375d1e4b61ca3e693f7ab05bf919
Saravanan J <60504057+saravanannnnn@users.noreply.github.com>
2020-05-30 00:26:56 +0530

    Update FC1.java

--- Practice/Flow Control Statements/FC1.java
+++ Practice/Flow Control Statements/FC1.java
@@ -1,1 +1,19 @@
+import java.util.Scanner;
 
+public class FC1 {
+
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		int x=s.nextInt();
+		if(x>0) {
+			System.out.println("Positive");
+		}
+		else if(x==0) {
+			System.out.println("Zero");
+		}
+		else {
+			System.out.println("Negative");
+		}
+		s.close();
+	}
+}
