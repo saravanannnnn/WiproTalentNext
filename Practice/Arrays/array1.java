76d8fec10ad830957445119265e0d709efa8bfa0
Saravanan J <60504057+saravanannnnn@users.noreply.github.com>
2020-06-02 00:49:52 +0530

    Update array1.java

--- Practice/Arrays/array1.java
+++ Practice/Arrays/array1.java
@@ -1,1 +1,20 @@
+import java.util.Scanner;
 
+public class Array1 {
+
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		int x=s.nextInt();
+		int sum=0;
+		float avg;
+		int[] arr=new int[x];
+		for(int i=0;i<x;i++) {
+			arr[i]=s.nextInt();
+			sum+=arr[i];
+		}
+		avg=sum/x;
+		System.out.println("Sum: "+sum);
+		System.out.println("Average: "+avg);
+		s.close();
+	}
+}
