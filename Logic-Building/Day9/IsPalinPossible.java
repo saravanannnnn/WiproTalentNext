19dfe5fe393d676986bf995ba541f975c11715c2
Saravanan J <60504057+saravanannnnn@users.noreply.github.com>
2020-06-09 23:11:15 +0530

    Update IsPalinPossible.java

--- Logic-Building/Day9/IsPalinPossible.java
+++ Logic-Building/Day9/IsPalinPossible.java
@@ -1,1 +1,32 @@
+import java.io.*;
+import java.util.*;
 
+class UserMainCode
+{
+	public int IsPalinPossible(int input1)
+	{
+		String s=String.valueOf(input1);
+		int x=0;
+		HashSet<Character> set=new HashSet<Character>();
+		for(int i=0;i<s.length();i++){
+			set.add(s.charAt(i));
+		}
+		for(Character ch:set){
+			int count=0;
+			for(int i=0;i<s.length();i++){
+				if(ch==s.charAt(i)){
+					count++;
+				}
+			}
+			if(count%2==1){
+				x++;
+			}
+		}
+		if(x>1){
+			return 1;
+		}
+		else{
+			return 2;
+		}
+	}
+}				
