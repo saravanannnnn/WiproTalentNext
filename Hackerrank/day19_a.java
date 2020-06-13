3d1152bffc9b055321f224a5f0658212749508a5
Saravanan J <60504057+saravanannnnn@users.noreply.github.com>
2020-06-13 18:31:27 +0530

    Create day19_a.java

---
+++ Hackerrank/day19_a.java
@@ -0,0 +1,47 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+
+
+class Result {
+
+    /*
+     * Complete the 'lastLetters' function below.
+     *
+     * The function is expected to return a STRING.
+     * The function accepts STRING word as parameter.
+     */
+
+    public static String lastLetters(String word) {
+    // Write your code here
+    String s=word.charAt(word.length()-1)+" "+word.charAt(word.length()-2);
+    return s;
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String word = bufferedReader.readLine();
+
+        String result = Result.lastLetters(word);
+
+        bufferedWriter.write(result);
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
